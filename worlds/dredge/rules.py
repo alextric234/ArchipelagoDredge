3d7797962d65b96ba57a9f687c225394542aff10
Alextric234 <randomguy234@gmail.com>
2025-05-08 13:37:25 -0500

    feat: added relics rule

--- worlds/dredge/rules.py
+++ worlds/dredge/rules.py
@@ -15,6 +15,15 @@ def set_region_rules(world: "DredgeWorld")
         lambda state: has_engines(1, state, player)
     world.get_entrance("Open Ocean -> The Pale Reach").access_rule \
         lambda state: has_engines(1, state, player)
+    world.get_entrance("Open Ocean -> Insanity").access_rule \
+        lambda state: has_relics(state, player)
 
 def has_engines(number: int, state: CollectionState, player: int)
     return state.has("Engine Tier 1", player)
+
+def has_relics(state: CollectionState, player: int)
+    return state.has("Relic 1", player) \
+    state.has("Relic 2", player) \
+    state.has("Relic 3", player) \
+    state.has("Relic 4", player) \
+    state.has("Relic 5", player) \
