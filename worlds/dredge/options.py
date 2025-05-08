91a5e3275562132296b5a904ad1c8234b38037a5
Alextric234 <randomguy234@gmail.com>
2025-05-08 10:27:32 -0500

    feat: added engine option

--- worlds/dredge/options.py
+++ worlds/dredge/options.py
@@ -11,8 +11,8 @@ from Options import Choice, Toggle, PerGameCommonOptions
 #     All Pursuits: Complete all pursuits
 #     All Fish Found: Fill out all applicable encyclopedia entries (Including DLCs will add those fish to this goal)
 #     """
-#     internal_name = 'goal'
-#     display_name = 'Goal'
+#     internal_name = "goal"
+#     display_name = "Goal"
 #     option_standard_ending = 0
 #     option_all_pursuits = 1
 #     option_all_fish_found = 2
@@ -22,36 +22,43 @@ class IncludeIronRigDLC(Toggle):
     """
     Include the Iron Rig DLC (Iron Rig DLC required for this)
     """
-    internal_name = 'include_iron_rig_dlc'
-    display_name = 'Include Iron Rig DLC'
+    internal_name = "include_iron_rig_dlc"
+    display_name = "Include Iron Rig DLC"
 
 # class RequireIronRigEnding(Toggle):
 #     """
-#     Require Iron Rig ending to have been achieved before game can be completed
+#     Require Iron Rig ending to have been achieved before game can be completed 
 #     """
-#     internal_name = 'require_iron_rig_ending'
-#     display_name = 'Require Iron Rig Ending'
+#     internal_name = "require_iron_rig_ending"
+#     display_name = "Require Iron Rig Ending"
 
 class IncludePaleReachDLC(Toggle):
     """
     Include the Pale Reach DLC (Pale Reach DLC required for this)
     """
-    internal_name = 'include_pale_reach_dlc'
-    display_name = 'Include Pale Reach DLC'
+    internal_name = "include_pale_reach_dlc"
+    display_name = "Include Pale Reach DLC"
 
 # class RequirePaleReachEnding(Toggle):
 #     """
 #     Require Pale Reach ending to have been achieved before game can be completed
 #     """
-#     internal_name = 'require_pale_reach_ending'
-#     display_name = 'Require Pale Reach Ending'
+#     internal_name = "require_pale_reach_ending"
+#     display_name = "Require Pale Reach Ending"
+
+class RequireEngines(Toggle)
+    """
+    Set logic to require engine upgrade to reach further archipelagos
+    """
+    internal_name = "require_engines"
+    display_name = "Require Engines"
 
 # class EnableTraps(Toggle):
 #     """
 #     Allow traps to be added to the pool to replace filler items
 #     """
-#     internal_name = 'enable_traps'
-#     display_name = 'Enable Traps'
+#     internal_name = "enable_traps"
+#     display_name = "Enable Traps"
 
 @dataclass
 class DredgeOptions(PerGameCommonOptions):
